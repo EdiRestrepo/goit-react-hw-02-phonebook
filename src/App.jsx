f9b4c2d53045fe805c194766722502c1344c8e57
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-06-04 16:59:13 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css'
+import { Component } from "react";
+import "./App.css";
 
-function App() {
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
 
-  return (
-    <>
-    <h1>Hola desde react</h1>
-    </>
-  )
+  render() {
+    return (
+      <>
+        <h1>Hola desde react</h1>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
